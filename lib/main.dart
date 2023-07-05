5ea76cd6bde0a61bf381d105d1642701f42a873a
yusefHajeb <watanalihsas@gmail.com>
2023-07-05 03:04:27 +0300

    imprt injection_container for bloc ,and clean main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:users_todo/core/theme/app_them.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:equatable/equatable.dart';
-// import 'package:users_todo/core/theme/app_them.dart';
+import 'package:users_todo/features/todos/presentation/bloc_todos/addDeleteUpdateTodo/add_delete_update_todo_bloc.dart';
+import 'package:users_todo/features/todos/presentation/bloc_todos/bloc/todos_bloc.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -13,15 +16,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Todo"),
-        ),
-        body: Center(
-          child: Text("Todos page"),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => di.sl<TodosBloc>()..add(GetAllTodoEvent())),
+        BlocProvider(create: (context) => di.sl<AddDeleteUpdateTodoBloc>()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Todo"),
+          ),
+          body: Center(
+            child: Text("Todos page"),
+          ),
         ),
       ),
     );

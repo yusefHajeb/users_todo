d974fdbf4f0f7cd3db0bad7229e11255e505ed50
yousefRafal <146264504+yousefRafal@users.noreply.github.com>
2024-01-26 02:10:21 +0300

    change  style todoWidget

--- lib/core/widget/todo_list_tail.dart
+++ lib/core/widget/todo_list_tail.dart
@@ -54,43 +54,59 @@ class TodoListTial extends StatelessWidget {
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Center(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                    child: Wrap(
+                        direction: Axis.vertical,
+                        crossAxisAlignment: WrapCrossAlignment.start,
+                        verticalDirection: VerticalDirection.down,
+                        alignment: WrapAlignment.spaceBetween,
+                        runAlignment: WrapAlignment.spaceBetween,
+                        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Row(children: [
-                            CircleAvatar(
-                              backgroundColor:
-                                  AppColors.lightMauveBackgroundColor,
-                              child: Container(
-                                  padding: const EdgeInsets.all(5),
-                                  width: 42,
-                                  height: 42,
-                                  decoration: BoxDecoration(
-                                      color:
-                                          AppColors.lightMauveBackgroundColor,
-                                      shape: BoxShape.circle),
-                                  child: Center(
-                                    child: Text(
-                                      todo.id.toString(),
-                                      textAlign: TextAlign.center,
-                                      style: context.textTheme.displayMedium!
-                                          .copyWith(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 15),
-                                    ),
-                                  )),
-                            ),
-                            AppSpaces.horizontalSpace20,
-                            Text(
-                              todo.title.toString(),
-                              style: GoogleFonts.lato(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 14),
-                            ),
-                          ]),
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundColor:
+                                    AppColors.lightMauveBackgroundColor,
+                                child: Container(
+                                    padding: const EdgeInsets.all(5),
+                                    width: 42,
+                                    height: 42,
+                                    decoration: BoxDecoration(
+                                        color:
+                                            AppColors.lightMauveBackgroundColor,
+                                        shape: BoxShape.circle),
+                                    child: Center(
+                                      child: Text(
+                                        todo.id.toString(),
+                                        textAlign: TextAlign.center,
+                                        style: context.textTheme.displayMedium!
+                                            .copyWith(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 15),
+                                      ),
+                                    )),
+                              ),
+                              AppSpaces.horizontalSpace20,
+                              Wrap(
+                                direction: Axis.vertical,
+                                crossAxisAlignment: WrapCrossAlignment.start,
+                                verticalDirection: VerticalDirection.down,
+                                alignment: WrapAlignment.spaceBetween,
+                                runAlignment: WrapAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    todo.title.toString(),
+                                    style: GoogleFonts.lato(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                           AppSpaces.horizontalSpace20,
                           Center(
                               child: todo.completed.toString() == "true"
